704218217cf931345bc5a1af3d64f210c00ee07f
파랑이 <parangee9706@gmail.com>
2021-04-04 17:34:00 +0900

    basic express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log('정현수 바부')
+import express from 'express'
+// @ts-ignore
+import config from '../config.json'
+
+const app = express()
+
+app.listen(config.port, () => console.log(`Listening on port ${config.port}`))
